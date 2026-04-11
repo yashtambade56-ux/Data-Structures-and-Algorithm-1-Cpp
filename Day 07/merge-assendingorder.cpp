c0fa4a3c129ad6ecf408bf2cd0f72a2950c36960
Yash tambade <yashtambade56@gmail.com>
2026-04-11 16:03:05 +0530

    Add files via upload

---
+++ Day 07/merge-assendingorder.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+void merge(int arr[], int l, int mid, int r) {
+    int n1 = mid - l + 1;
+    int n2 = r - mid;
+
+        int left[n1], right[n2];
+
+        for (int i = 0; i < n1; i++)
+            left[i] = arr[l + i];
+        for (int i = 0; i < n2; i++)
+            right[i] = arr[mid + 1 + i];
+
+        int i = 0, j = 0, k = l;
+        while (i < n1 && j < n2) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+}
+
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int mid = l + (r - l) / 2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    mergeSort(arr, 0, n - 1);
+
+    cout << "Sorted array: \n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+    return 0;
+}
